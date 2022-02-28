f467d211c174884240a061fb20933936622b853b
Rio Chandra <riochandra4@gmail.com>
2022-02-28 16:15:22 +0700

    add siswa module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SiswaModule } from './siswa/siswa.module';
 
 @Module({
-  imports: [],
+  imports: [SiswaModule],
   controllers: [AppController],
   providers: [AppService],
 })
